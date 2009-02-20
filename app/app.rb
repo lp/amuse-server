8be82bc8fb14d7bf02416a5ee5352157633531c9
lp <lp@spiralix.org>
2009-02-20 12:58:21 -0500

    cleanup and add message_store

--- app/app.rb
+++ app/app.rb
@@ -1,28 +1,10 @@
 require 'app/helpers/projects'
 require 'app/helpers/crypt'
-
-class Counter
-	
-	@@count = 0
-	
-	def Counter::num
-		@@count += 1
-		@@count.to_s
-	end
-	
-end
+require 'app/message_store'
 
 helpers do
 	include Projects
 	include Crypt
-end
-
-get '/' do
-	"Worked on Dreamhost: #{Counter::num}"
-end
-
-get '/foo/:bar/:goo' do
-	"You asked for foo/#{params[:bar]}/#{params[:goo]}"
 end
 
 get '/projects' do
