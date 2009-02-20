7ff1534c0a5f9975739b25ee69fa74dda0326bf2
lp <lp@spiralix.org>
2009-02-20 13:42:59 -0500

    cache dashboard and thread

--- app/helpers/serial_cache.rb
+++ app/helpers/serial_cache.rb
@@ -6,10 +6,18 @@ class SerialCache
 	
 	CACHE_ROOT = 'app/data/cache'
 	
-	def self.dashboard(dash_array)
+	def self.dashboard(dataset)
+		last_id = dataset.last.map(:id); first_id = last_id - 10; first_id = 0 if first_id < 0
 		File.open(Path.catname(CACHE_ROOT,'dashboard.cache'),'w') do |file|
-			file.puts(encrypt(YAML::dump(dash_array)))
+			file.puts( encrypt( YAML::dump( @db[:store].filter(:id => first_id..last_id).map )))
 		end
+	end
+	
+	def self.thread(dataset)
+		thread_array = dataset.map
+		cache_path = Path.catname(CACHE_ROOT,"#{thread_array[0][:project_id].to_s}/#{thread_array[0][:thread_id].to_s}.cache")
+		Path.make(Path.dir(cache_path))
+		File.open(cache_path,'w') { |file| file.puts( encrypt( YAML::dump( thread_array ))) }
 	end
 	
 	
