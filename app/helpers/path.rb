e5a66bbbfcf9a6ba8475e4261ee6ac6c3112fbb3
lp <lp@spiralix.org>
2009-02-20 13:41:51 -0500

    .dir and .make

--- app/helpers/path.rb
+++ app/helpers/path.rb
@@ -1,9 +1,18 @@
 module Path
+	require 'ftools'
 	
 	def Path.catname(first,last)
 		first << '/' if first[/\/$/].nil?
 		last[0] = '' if ! last[/^\//].nil?
 		first + last
+	end
+	
+	def Path.dir(path)
+		File.dirname(path)
+	end
+	
+	def Path.make(path)
+		File.makedirs(path)
 	end
 	
 end
