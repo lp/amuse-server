1806f91f20ed69b367d9122b03d8887f89fcdf55
lp <lp@spiralix.org>
2009-02-20 18:40:57 -0500

    MessageStore becomes a module

--- app/message_store.rb
+++ app/message_store.rb
@@ -1,57 +1,60 @@
-class MessageStore
+module MessageStore
 	require 'sequel'
 	require 'app/helpers/serial_cache'
+	DB_PATH = 'app/data/db/message_store.db'
 	
-	def initialize(path)
-		store = path + '.blob'
-		@db = Sequel.sqlite(store)
-		begin
-			@db.create_table :store do
-				primary_key :id
-				column :project_id, :integer
-				column :project, :text
-				column :thread_id, :integer
-				column :subject, :text 
-				column :author, :text
-				column :datetime, :datetime
-				column :title, :text
-				column :message, :text
-			end
-			@db.create_table :threads do
-				primary_key :id
-				column :subject, :text
-				column :project_id, :integer 
-			end
-			@db.create_table :projects do
-				primary_key :id
-				column :name, :text
-			end
-		rescue Sequel::DatabaseError
+	@@db = Sequel.sqlite(DB_PATH)
+	begin
+		@@db.create_table :messages do
+			primary_key :id
+			column :project_id, :integer
+			column :project, :text
+			column :thread_id, :integer
+			column :subject, :text 
+			column :author, :text
+			column :datetime, :datetime
+			column :title, :text
+			column :message, :text
 		end
+	rescue Sequel::DatabaseError
+	end
+	begin
+		@@db.create_table :threads do
+			primary_key :id
+			column :subject, :text
+			column :project_id, :integer 
+		end
+	rescue Sequel::DatabaseError
+	end
+	begin
+		@@db.create_table :projects do
+			primary_key :id
+			column :name, :text
+			column :description, :text
+		end
+		rescue Sequel::DatabaseError
 	end
 	
-	def self.open(path)
-		self.new(path)
+	def MessageStore.read_thread(id)
+		@@db[:messages].where(:thread_id => id).reverse_order(:id).map
 	end
 	
-	def read_thread(id)
-		@db[:store].where(:thread_id => id).reverse_order(:id).map
+	def MessageStore.new_message(row_hash)
+		@@db[:messages] << row_hash
+		SerialCache.dashboard(@@db[:messages])
+		SerialCache.messages(@@db[:messages].where(:thread_id => row_hash[:thread_id]))
 	end
 	
-	def new_message(row_hash)
-		@db[:store] << row_hash
-		SerialCache.dashboard(@db[:store])
-		SerialCache.thread(@db[:store].where(:thread_id => row_hash[:thread_id]))
+	def MessageStore.new_thread(subject,project_id)
+		@@db[:threads] << {:subject => subject, :project_id => project_id}
+		SerialCache.threads(@@db[:threads].where(:project_id => project_id))
+		@@db[:threads].where(:subject => subject).map(:id).last
 	end
 	
-	def new_project(name)
-		@db[:projects] << {:name => name}
-		@db[:projects].where(:name => name).map(:id).last
-	end
-	
-	def new_thread(subject,project_id)
-		@db[:threads] << {:subject => subject, :project_id => project_id}
-		@db[:threads].where(:subject => subject).map(:id).last
+	def MessageStore.new_project(name)
+		@@db[:projects] << {:name => name}
+		SerialCache.projects(@@db[:projects])
+		@@db[:projects].where(:name => name).map(:id).last
 	end
 	
 end
