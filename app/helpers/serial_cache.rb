2f58ade296f9641f65969cc81280f0ca2779684e
lp <lp@spiralix.org>
2009-02-20 18:41:53 -0500

    threads and project cache

--- app/helpers/serial_cache.rb
+++ app/helpers/serial_cache.rb
@@ -2,23 +2,37 @@ class SerialCache
 	require 'yaml'
 	require 'app/helpers/path'
 	require 'app/helpers/crypt'
-	include Crypt
 	
 	CACHE_ROOT = 'app/data/cache'
 	
 	def self.dashboard(dataset)
 		last_id = dataset.last.map(:id); first_id = last_id - 10; first_id = 0 if first_id < 0
 		File.open(Path.catname(CACHE_ROOT,'dashboard.cache'),'w') do |file|
-			file.puts( encrypt( YAML::dump( @db[:store].filter(:id => first_id..last_id).map )))
+			file.puts( Crypt.encrypt( YAML::dump( dataset.filter(:id => first_id..last_id).map )))
 		end
 	end
 	
-	def self.thread(dataset)
-		thread_array = dataset.map
-		cache_path = Path.catname(CACHE_ROOT,"#{thread_array[0][:project_id].to_s}/#{thread_array[0][:thread_id].to_s}.cache")
+	def self.messages(dataset)
+		message_threads = dataset.map
+		cache_path = Path.catname(CACHE_ROOT,"projects/#{message_threads[0][:project_id].to_s}/#{message_threads[0][:thread_id].to_s}.cache")
 		Path.make(Path.dir(cache_path))
-		File.open(cache_path,'w') { |file| file.puts( encrypt( YAML::dump( thread_array ))) }
+		File.open(cache_path,'w') { |file| file.puts( Crypt.encrypt( YAML::dump( message_threads ))) }
 	end
+	
+	def self.threads(dataset)
+		project_threads = dataset.map
+		cache_path = Path.catname(CACHE_ROOT,"projects/#{project_threads[0][:project_id].to_s}.cache")
+		Path.make(Path.dir(cache_path))
+		File.open(cache_path,'w') { |file| file.puts( Crypt.encrypt( YAML::dump( project_threads ))) }
+	end
+	
+	def self.projects(dataset)
+		File.open(Path.catname(CACHE_ROOT,'projects.cache'),'w') do |file|
+			file.puts( Crypt.encrypt( YAML::dump( dataset.map)))
+		end
+	end
+	
+	
 	
 	
 end
