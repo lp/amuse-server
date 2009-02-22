b60ddb5ddd2cd367a6c6fe2ff835e4ca36a5feec
lp <lp@spiralix.org>
2009-02-21 19:45:40 -0500

    to_s before encrypt

--- app/app.rb
+++ app/app.rb
@@ -10,18 +10,18 @@ helpers do
 	include AmuseHelpers
 end
 
-post '/authors' do
-	Crypt.encrypt( MessageStore.new_author( YAML::load( Crypt.decrypt( params[:o]))))
+post '/authors/new' do
+	Crypt.encrypt( MessageStore.new_author( YAML::load( Crypt.decrypt( params[:o]))).to_s)
 end
 
-post '/projects' do
-	Crypt.encrypt( MessageStore.new_project( YAML::load( Crypt.decrypt( params[:o]))))
+post '/projects/new' do
+	Crypt.encrypt( MessageStore.new_project( YAML::load( Crypt.decrypt( params[:o]))).to_s)
 end
 
-post '/threads' do
-	Crypt.encrypt( MessageStore.new_thread( YAML::load( Crypt.decrypt( params[:o]))))
+post '/threads/new' do
+	Crypt.encrypt( MessageStore.new_thread( YAML::load( Crypt.decrypt( params[:o]))).to_s)
 end
 
-post '/messages' do
-	Crypt.encrypt( MessageStore.new_message( YAML::load( Crypt.decrypt( params[:o]))))
+post '/messages/new' do
+	Crypt.encrypt( MessageStore.new_message( YAML::load( Crypt.decrypt( params[:o]))).to_s)
 end
