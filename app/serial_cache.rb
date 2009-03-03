d2fa7a23014a876f5b22ef1ecfa32f7efec1c5d7
lp <lp@spiralix.org>
2009-03-03 13:33:55 -0500

    simplified path creation with File.join

--- app/serial_cache.rb
+++ app/serial_cache.rb
@@ -3,43 +3,43 @@ class SerialCache
 	require File.join( File.dirname( File.expand_path(__FILE__)), 'helpers','path')
 	require File.join( File.dirname( File.expand_path(__FILE__)), 'helpers','crypt')
 	
-	CACHE_ROOT = 'data/cache'
+	CACHE_ROOT = File.join( File.dirname( File.expand_path(__FILE__)), '..','data','cache')
 	
-	unless File.exist?( Path.catname(CACHE_ROOT,'empty.cache'))
-		File.open(Path.catname(CACHE_ROOT,'empty.cache'),'w') do |file|
+	unless File.exist?( File.join(CACHE_ROOT,'empty.cache'))
+		File.open( File.join(CACHE_ROOT,'empty.cache'),'w') do |file|
 			file.puts( Crypt.encrypt( YAML::dump( [{}])))
 		end
 	end
 	
 	def self.authors(dataset)
-		File.open(Path.catname(CACHE_ROOT,'authors.cache'),'w') do |file|
+		File.open( File.join(CACHE_ROOT,'authors.cache'),'w') do |file|
 			file.puts( Crypt.encrypt( YAML::dump( dataset.map)))
 		end
 	end
 	
 	def self.dashboard(dataset)
 		last_id = dataset.order(:id).last[:id]; first_id = last_id - 10; first_id = 0 if first_id < 0
-		File.open(Path.catname(CACHE_ROOT,'dashboard.cache'),'w') do |file|
+		File.open( File.join(CACHE_ROOT,'dashboard.cache'),'w') do |file|
 			file.puts( Crypt.encrypt( YAML::dump( dataset.filter(:id => first_id..last_id).map )))
 		end
 	end
 	
 	def self.messages(dataset)
 		message_threads = dataset.map
-		cache_path = Path.catname(CACHE_ROOT,"projects/#{message_threads[0][:project_id].to_s}/#{message_threads[0][:thread_id].to_s}.cache")
+		cache_path = File.join(CACHE_ROOT,"projects/#{message_threads[0][:project_id].to_s}/#{message_threads[0][:thread_id].to_s}.cache")
 		Path.make(Path.dir(cache_path))
 		File.open(cache_path,'w') { |file| file.puts( Crypt.encrypt( YAML::dump( message_threads ))) }
 	end
 	
 	def self.threads(dataset)
 		project_threads = dataset.map
-		cache_path = Path.catname(CACHE_ROOT,"projects/#{project_threads[0][:project_id].to_s}.cache")
+		cache_path = File.join(CACHE_ROOT,"projects/#{project_threads[0][:project_id].to_s}.cache")
 		Path.make(Path.dir(cache_path))
 		File.open(cache_path,'w') { |file| file.puts( Crypt.encrypt( YAML::dump( project_threads ))) }
 	end
 	
 	def self.projects(dataset)
-		File.open(Path.catname(CACHE_ROOT,'projects.cache'),'w') do |file|
+		File.open( File.join(CACHE_ROOT,'projects.cache'),'w') do |file|
 			file.puts( Crypt.encrypt( YAML::dump( dataset.map)))
 		end
 	end
