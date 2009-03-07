fda6bfbf58ba3b30c72a28e547c250196ef72074
lp <lp@spiralix.org>
2009-03-07 15:29:47 -0500

    made into an app and an insertable middleware

--- app/auth.rb
+++ app/auth.rb
@@ -1,21 +1,40 @@
 require File.join( File.dirname( File.expand_path(__FILE__)), 'key_store')
 
-class AuthDoor
-	
-	def call(env)
-		req = Rack::Request.new(env)
-		
-		
-	end
-	
-end
-
 class AuthKeys
 	
 	def call(env)
 		req = Rack::Request.new(env)
-		
+		if req[:r].nil?
+			KeyStore.challenge(req[:a])
+		else
+			if Keystore.response?(req[:a],req[:r])
+				keys = KeyStore.author_keys(req[:a])
+				[200, {	'Content-Type' => 'text/plain', 'Content-Length' => keys.size.to_s},keys]
+			else
+				[403, headers, "Go Away!!"]
+			end
+		end
 	end
 	
+end
+
+module Rack
+	class AuthDoor
+		
+		def initialize app
+			@app = app
+		end
+
+		def call(env)
+			req = Rack::Request.new(env)
+			status, headers, body = @app.call env
+			if KeyStore.authorized?(req[:a],req[:k])
+				[status, headers, body]
+			else
+				[403, headers, "Go Away!!"]
+			end
+		end
+
+	end
 end
 	
