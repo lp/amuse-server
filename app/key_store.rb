081faee67625611b9ae7ab6fd31d71f89032d911
lp <lp@spiralix.org>
2009-03-07 13:00:29 -0500

    key_store challenge and response

--- app/key_store.rb
+++ app/key_store.rb
@@ -2,8 +2,18 @@ module KeyStore
 	require File.join( File.dirname( File.expand_path(__FILE__)), '..', 'vendor', 'sequel', 'lib', 'sequel')
 	require File.join( File.dirname( File.expand_path(__FILE__)), 'helpers', 'random')
 	require File.join( File.dirname( File.expand_path(__FILE__)), 'helpers', 'crypt')
+	require File.join( File.dirname( File.expand_path(__FILE__)), 'helpers', 'challenge')
 	DB_PATH = File.join( File.dirname( File.expand_path(__FILE__)), '..', 'data', 'db', 'key_store.db')
 	@@db = Sequel.sqlite(DB_PATH)
+	begin
+		@@db.create_table :challenge do
+			primary_key :id
+			column :created, :integer
+			column :author_id, :integer
+			column :response, :integer
+		end
+	rescue Sequel::DatabaseError
+	end
 	
 	def KeyStore.author_keys(author_id)
 		keys = Random.keys(256,256)
@@ -19,6 +29,24 @@ module KeyStore
 		if result == 0
 			return false
 		elsif result > 0
+			return true
+		else
+			return false
+		end
+	end
+	
+	def KeyStore.challenge(author_id)
+		@@db[:challenge].filter(:author_id => author_id).delete
+		c = Challenge.new
+		@@db[:challenge] << {:created => Time.now.to_i, :author_id => author_id, :response => c.response}
+		Crypt.encrypt( YAML::dump( c.challenge))
+	end
+	
+	def Keystore.response?(author_id,response)
+		row = @@db[:challenge].filter(:author_id => author_id)
+		if Time.now.to_i > (row.map(:created)+50)
+			return false
+		elsif row.map(:response) == response
 			return true
 		else
 			return false
