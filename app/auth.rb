d58224a316494caaf0a20d05c0cfd61c40d182d4
lp <lp@spiralix.org>
2009-03-08 14:05:26 -0400

    AuthKeys support for temp ipkey

--- app/auth.rb
+++ app/auth.rb
@@ -1,15 +1,23 @@
 require File.join( File.dirname( File.expand_path(__FILE__)), 'key_store')
-
+# Author:: lp (mailto:lp@spiralix.org)
+# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+# 
+# :title:amuse-server/auth
 class AuthKeys
 	
 	def call(env)
-		req = Rack::Request.new(env)
+		req = Rack::Request.new(env)	
 		if req[:r].nil?
 			KeyStore.challenge(req[:a])
 		else
 			if Keystore.response?(req[:a],req[:r])
-				keys = KeyStore.author_keys(req[:a])
-				[200, {	'Content-Type' => 'text/plain', 'Content-Length' => keys.size.to_s},keys]
+				if req[:a] == 'ipkey'
+					key = KeyStore.ip_key(req.ip)
+					[200, {	'Content-Type' => 'text/plain', 'Content-Length' => key.size.to_s},key]
+				else
+					keys = KeyStore.author_keys(req[:a])
+					[200, {	'Content-Type' => 'text/plain', 'Content-Length' => keys.size.to_s},keys]
+				end
 			else
 				[403, headers, "Go Away!!"]
 			end
@@ -19,6 +27,7 @@ class AuthKeys
 end
 
 module Rack
+
 	class AuthDoor
 		
 		def initialize app
