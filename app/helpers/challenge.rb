58cbfdf1d9167d447a3a962230719c3339d5668b
lp <lp@spiralix.org>
2009-03-08 21:41:36 -0400

    floted the first eval number to float operation

--- app/helpers/challenge.rb
+++ app/helpers/challenge.rb
@@ -7,8 +7,8 @@ class Challenge
 	
 	attr_reader :challenge, :response
 	def initialize
-		@challenge = [Random.operator, Random.number( rand(4)+2), Random.number( rand(4)+2)]
-		@response = eval(@challenge[1].to_s+@challenge[0]+@challenge[2].to_s).to_s
+		@challenge = [Random.operator, Random.number( (rand(4)+2)), Random.number( (rand(4)+2))]
+		@response = eval(@challenge[1].to_s+'.0'+@challenge[0]+@challenge[2].to_s).to_s
 	end
 	
 end
