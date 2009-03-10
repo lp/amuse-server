84aea6f57e7c00ec07774b6fb881e5429abf79dd
lp <lp@spiralix.org>
2009-03-10 09:16:40 -0400

    let the environment create the empty.cache

--- app/serial_cache.rb
+++ app/serial_cache.rb
@@ -9,12 +9,6 @@ class SerialCache
 	
 	CACHE_ROOT = File.join( File.dirname( File.expand_path(__FILE__)), '..','data','cache')
 	
-	unless File.exist?( File.join(CACHE_ROOT,'empty.cache'))
-		File.open( File.join(CACHE_ROOT,'empty.cache'),'w') do |file|
-			file.puts( Crypt.encrypt( YAML::dump( [{}])))
-		end
-	end
-	
 	def self.authors(dataset)
 		File.open( File.join(CACHE_ROOT,'authors.cache'),'w') do |file|
 			file.puts( Crypt.encrypt( YAML::dump( dataset.map)))
